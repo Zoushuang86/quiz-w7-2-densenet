aaeb1c77dfdfe330bafc79a426d306520d61ba81
Zoushuang86 <33125245+Zoushuang86@users.noreply.github.com>
2018-09-17 20:32:05 +0800

    Update dataset_factory.py

--- datasets/dataset_factory.py
+++ datasets/dataset_factory.py
@@ -23,6 +23,7 @@ from datasets import flowers
 from datasets import imagenet
 from datasets import mnist
 from datasets import quiz
+from datasets import car
 
 datasets_map = {
     'cifar10': cifar10,
