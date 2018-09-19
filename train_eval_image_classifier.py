93b507c8e0b321b89fdd167584de50eefc7a1ffd
Zoushuang86 <33125245+Zoushuang86@users.noreply.github.com>
2018-09-19 11:09:28 +0800

    Update train_eval_image_classifier.py

--- train_eval_image_classifier.py
+++ train_eval_image_classifier.py
@@ -50,7 +50,7 @@ if __name__ == '__main__':
         # train 1 epoch
         print('################    train    ################')
         p = os.popen(train_cmd.format(**{'dataset_name': FLAGS.dataset_name, 'dataset_dir': FLAGS.dataset_dir,
-                                         'model_name': FLAGS. model_name,
+                                         'model_name': FLAGS. model_name,'dataset_split_name': 'train',
                                          'checkpoint_exclude_scopes': FLAGS.checkpoint_exclude_scopes, 'train_dir': FLAGS. train_dir,
                                          'learning_rate': FLAGS.learning_rate, 'optimizer': FLAGS.optimizer,
                                          'batch_size': FLAGS.batch_size, 'max_number_of_steps': steps, 'clone_on_cpu': FLAGS.clone_on_cpu}) + ckpt)
